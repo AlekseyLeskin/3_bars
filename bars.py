a0bb088c43843bd0cf26685bab3adf3d55145c94
a.leskin <a.leskin@a.leskin>
2018-01-29 09:57:09 +0300

    fixed pep8 and names of variables

--- bars.py
+++ bars.py
@@ -7,13 +7,17 @@ def load_data(file_path):
         return json.load(open_file)
 
 
-def get_biggest_bar(data):
-    biggest_bar = max(data['features'], key=lambda seats_count: seats_count['properties']['Attributes']['SeatsCount'])
+def get_biggest_bar(restaurants_list):
+    biggest_bar = max(restaurants_list['features'],
+                      key=lambda seats_count:
+                      seats_count['properties']['Attributes']['SeatsCount'])
     return biggest_bar['properties']['Attributes']['Name']
 
 
-def get_smallest_bar(data):
-    smallest_bar = min(data['features'], key=lambda seats_count: seats_count['properties']['Attributes']['SeatsCount'])
+def get_smallest_bar(restaurants_list):
+    smallest_bar = min(restaurants_list['features'],
+                       key=lambda seats_count:
+                       seats_count['properties']['Attributes']['SeatsCount'])
     return smallest_bar['properties']['Attributes']['Name']
 
 
@@ -21,11 +25,13 @@ def calc_distance(x1, y1, x2, y2):
     return ((x2 - x1) ** 2) + ((y2 - y1) ** 2) ** 0.5
 
 
-def get_closest_bar(data, longitude, latitude):
-    nearest_bar = min(data['features'], key=lambda restaurant: calc_distance(longitude,
-                                                                             latitude,
-                                                                             restaurant['geometry']['coordinates'][0],
-                                                                             restaurant['geometry']['coordinates'][1]))
+def get_closest_bar(restaurants_list, longitude, latitude):
+    nearest_bar = min(restaurants_list['features'],
+                      key=lambda restaurant:
+                      calc_distance(longitude,
+                                    latitude,
+                                    restaurant['geometry']['coordinates'][0],
+                                    restaurant['geometry']['coordinates'][1]))
     return nearest_bar['properties']['Attributes']['Name']
 
 
@@ -38,7 +44,12 @@ if __name__ == '__main__':
         file_data = load_data(user_file_path)
         print('Самый большой бар', get_biggest_bar(file_data))
         print('Самый маленький бар', get_smallest_bar(file_data))
-        user_longitude, user_latitude = input('\nВведите через пробел координаты текущего местоположения: \n').split(" ")
-        print('Самый близкий бар:', str(get_closest_bar(file_data, float(user_longitude), float(user_latitude))))
+        user_longitude, user_latitude = \
+            input('\nВведите через пробел '
+                  'координаты текущего местоположения: \n').split(" ")
+        print('Самый близкий бар:',
+              str(get_closest_bar(file_data,
+                                  float(user_longitude),
+                                  float(user_latitude))))
     except FileNotFoundError as e:
         print("Файла не существует")
