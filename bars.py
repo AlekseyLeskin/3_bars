ee89981dd30096ab0a6707ce0190965be6b8f0a9
a.leskin <a.leskin@a.leskin>
2018-01-30 18:15:46 +0300

    fixed comments

--- bars.py
+++ bars.py
@@ -45,41 +45,40 @@ def get_closest_bar(data_for_restaurants, longitude, latitude):
     return nearest_bar
 
 
-try:
-    if len(sys.argv) == 1:
-        user_file_path = input('Укажите путь до файла: ')
-    else:
-        user_file_path = sys.argv[1]
+if __name__ == '__main__':
     try:
+        if len(sys.argv) == 1:
+            user_file_path = input('Укажите путь до файла: ')
+        else:
+            user_file_path = sys.argv[1]
         list_of_restaurants = load_data(user_file_path)['features']
-    except json.decoder.JSONDecodeError as e:
-        print('Некорректный JSON')
-        raise SystemExit(1)
-    print(
-        'Самый большой бар',
-        get_biggest_bar(list_of_restaurants)
-        ['properties']['Attributes']['Name'],
-    )
-    print(
-        'Самый маленький бар',
-        get_smallest_bar(list_of_restaurants)
-        ['properties']['Attributes']['Name'],
-    )
-    try:
+        print(
+            'Самый большой бар',
+            get_biggest_bar(list_of_restaurants)
+            ['properties']['Attributes']['Name'],
+        )
+        print(
+            'Самый маленький бар',
+            get_smallest_bar(list_of_restaurants)
+            ['properties']['Attributes']['Name'],
+        )
         user_longitude, user_latitude = [float(point) for point in input(
             '\nВведите через пробел '
             'координаты текущего местоположения: \n'
         ).split(' ')]
-
-    except (ValueError, TypeError) as e:
+        print('Самый близкий бар:', str(get_closest_bar(
+                                            list_of_restaurants,
+                                            float(user_longitude),
+                                            float(user_latitude),
+                                        )['properties']['Attributes']['Name']))
+    except FileNotFoundError:
+        print('Файла не существует')
+        exit()
+    except json.decoder.JSONDecodeError:
+        print('Некорректный JSON')
+        exit()
+    except (ValueError, TypeError):
         print('\nНекорректный формат координат.'
               '\nПример корректного ввода: '
               '"37.635709999610896 55.805575000158512" ')
-        raise SystemExit(1)
-    print('Самый близкий бар:', str(get_closest_bar(
-                                        list_of_restaurants,
-                                        float(user_longitude),
-                                        float(user_latitude),
-                                    )['properties']['Attributes']['Name']))
-except FileNotFoundError as e:
-    print('Файла не существует')
+        exit()
